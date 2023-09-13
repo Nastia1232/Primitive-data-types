a21b900861dc97930b13e202c1bfd429c11816d5
nastia.kuz <nastenanastena180@gmail.com>
2023-09-13 22:30:05 +0300

    Корректные значения входных данных

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double ticketPrice = 13676;
+        double milesPerRuble = 20;
+
+
+        int milesEarned = (int) (ticketPrice / milesPerRuble);
+
+
+        System.out.println("За билет стоимостью " + ticketPrice + " рублей начислено " + milesEarned + " миль.");
     }
 }
